89951f4d051b016b29da4dd879c1e0451809a5ad
Kenzo Friás <kenzofrias16@gmail.com>
2026-05-22 11:24:19 -0300

    test: conclui testes iniciais de animal

--- PetVacinas.Tests/AnimalTests.cs
+++ PetVacinas.Tests/AnimalTests.cs
@@ -4,16 +4,29 @@ using System.Linq;
 using System.Threading.Tasks;
 using PetVacinas.Core.Services;
 using PetVacinas.Core.Models;
+using PetVacinas.Core.Interfaces;
 using Xunit;
 
 namespace PetVacinas.Tests
 {
     public class AnimalTests
     {
+        private readonly IVacina _v1 = new Vacina("Antirrábica", "Sanofi Pasteur");
+        private readonly IVacina _v2 = new Vacina("Vacina Multipla", "Fundação Butantan");
+
+        private readonly IAnimal _a1 = new Animal("Rex", "Labrador", "João");
+        private readonly IAnimal _a2 = new Animal("Biscoito", "Caramelo", "Sabrina");
+        private readonly IAnimal _a3 = new Animal("Dolly", "Vira-lata", "André");
+
+        private readonly IVacinacao _vac1;
+        private readonly IVacinacao _vac2;
+
         private readonly AnimalService _animalService;
         public AnimalTests()
         {
             _animalService = new AnimalService();
+            _vac1 = new Vacinacao("Carlos", _v1);
+            _vac2 = new Vacinacao("Jorge", _v2);
         }
 
         // Sucessos
@@ -21,7 +34,7 @@ namespace PetVacinas.Tests
         public void DeveCadastrarAnimalComSucessoNoRepositorio()
         {
             //Arrage
-            var animal = new Animal("Rex", "Labrador", "João");
+            var animal = _a1;
             //Act
             var result = _animalService.CadastrarAnimal(animal);
             //Assert
@@ -32,7 +45,7 @@ namespace PetVacinas.Tests
         public void DeveObterAnimalPeloSeuId()
         {
             //Arrage
-            var animal = new Animal("Biscoito", "Caramelo", "Sabrina");
+            var animal = _a2;
             var animalCadastrado = _animalService.CadastrarAnimal(animal);
             //Act
             var result = _animalService.ObterAnimalPorId(animalCadastrado.Id);
@@ -44,7 +57,7 @@ namespace PetVacinas.Tests
         public void DeveMostrarHistoricoDeVacinacoesDoAnimalVazio()
         {
             //Arrage
-            var animal = new Animal("Dolly", "Vira-lata", "André");
+            var animal = _a3;
             var animalCadastrado = _animalService.CadastrarAnimal(animal);
             //Act
             _animalService.ObterHistoricoDeVacinacoes(animalCadastrado.Id);
@@ -56,9 +69,9 @@ namespace PetVacinas.Tests
         public void DeveObterTodosOsAnimaisCadastradosEPresentesNoRepositorio()
         {
             //Arrage
-            var animal1 = new Animal("Luna", "Poodle", "Maria");
-            var animal2 = new Animal("Max", "Bulldog", "Carlos");
-            var animal3 = new Animal("Bella", "Golden Retriever", "Ana");
+            var animal1 = _a1;
+            var animal2 = _a2;
+            var animal3 = _a3;
             //Act
             _animalService.CadastrarAnimal(animal1);
             _animalService.CadastrarAnimal(animal2);
@@ -67,16 +80,46 @@ namespace PetVacinas.Tests
             //Assert
             Assert.NotEmpty(result);
             Assert.Equal(3, result.Count());
-            Assert.Contains(result, a => a.Nome == "Luna");
-            Assert.Contains(result, a => a.Nome == "Max");
-            Assert.Contains(result, a => a.Nome == "Bella");
+            Assert.Contains(result, a => a.Nome == "Rex");
+            Assert.Contains(result, a => a.Nome == "Biscoito");
+            Assert.Contains(result, a => a.Nome == "Dolly");
         }
 
-        // [Fact] DeveMostrarHistoricoDeVacinacoesDoAnimalComVacinacoesRegistradas
-        // NoEmpty, Equal e Contains para comparações
+        [Fact]
+        public void DeveRegistrarVacinacaoComSucessoNoAnimal()
+        {
+            //Arrange
+            var animal = _a3;
+            var vacinacao = _vac1;
 
-        // [Fact] DeveRegistrarVacinacaoComSucessoNoAnimal
-        // Equal para comparar a vacinação registrada com a vacinação retornada no histórico do animal
+            //Act
+            _animalService.CadastrarAnimal(animal);
+            var result = _animalService.RegistrarVacinacao(animal.Id, vacinacao);
+
+            //Assert
+            Assert.Equal(vacinacao, result);
+        }
+        
+        [Fact] 
+        public void DeveMostrarHistoricoDeVacinacoesDoAnimalComVacinacoesRegistradas()
+        {
+            //Arrange
+            var animal = _a2;
+            var vacinacao1 = _vac1;
+            var vacinacao2 = _vac2;
+
+            //Act
+            _animalService.CadastrarAnimal(animal);
+            _animalService.RegistrarVacinacao(animal.Id, vacinacao1);
+            _animalService.RegistrarVacinacao(animal.Id, vacinacao2);
+            var result = _animalService.ObterHistoricoDeVacinacoes(animal.Id);
+
+            //Assert
+            Assert.NotEmpty(result);
+            Assert.Equal(2, result.Count());
+            Assert.Contains(result, v => v.Vacina == _v1);
+            Assert.Contains(result, v => v.Vacina == _v2);
+        }
 
         // Falhas
         [Fact]
@@ -92,7 +135,7 @@ namespace PetVacinas.Tests
         public void DeveRetornarExceçãoAoTentarCadastrarAnimalComIdJaExistente()
         {
             //Arrange
-            var animal = new Animal("Rex", "Labrador", "João");
+            var animal = _a1;
             //Act
             _animalService.CadastrarAnimal(animal);
             //Assert
@@ -119,7 +162,7 @@ namespace PetVacinas.Tests
         public void DeveRetornarExcecaoAoTentarRegistrarVacinacaoNula()
         {
             // Arrage
-            var animal = new Animal("Rex", "Labrador", "João");
+            var animal = _a1;
             Vacinacao vacinacaoNula = null;
             // Act
             _animalService.CadastrarAnimal(animal);
